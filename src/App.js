0c2b645c18f6330b4c273cc033f4a05a59bed7aa
--replace-all <tristanbecnel87@gmail.com>
2023-04-10 11:26:43 -0500

    aws testing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <b> Welcome to AWS</b>
         <a
           className="App-link"
           href="https://reactjs.org"
