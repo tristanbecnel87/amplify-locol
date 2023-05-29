8ae7e59cb5d9b844473c83e136bdcc3edbe90766
--replace-all <tristanbecnel87@gmail.com>
2023-05-29 13:52:06 -0500

    added isbusiness field and started accessing attributes

--- src/frontend/login/login.js
+++ src/frontend/login/login.js
@@ -4,6 +4,7 @@ import {Authenticator, useAuthenticator, View} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import {useNavigate, useLocation} from 'react-router';
+import { Auth } from 'aws-amplify';
 
 
  const formFields = {
@@ -25,13 +26,22 @@ import {useNavigate, useLocation} from 'react-router';
           placeholder: 'Enter your Business Name, else type N/A',
           order: 4
       },
+
+      'custom:isbusiness': {
+          key:'isbusiness',
+          type:'string',
+          required:true,
+          label:'Are you registering for a business account?',
+          placeholder: 'Type Yes or No',
+          order: 5
+      },
         'custom:university': {
           key:'university',
           type:'string',
           required:true,
           label:'What university do you attend? ',
           placeholder: 'Type Here',
-          order: 5
+          order: 6
       },
          'custom:study': {
           key:'study',
@@ -39,7 +49,7 @@ import {useNavigate, useLocation} from 'react-router';
           required:true,
           label:'What do you study? ',
           placeholder: 'Type Here',
-          order: 6
+          order: 7
       },
          'custom:classification': {
           key:'classification',
@@ -47,7 +57,7 @@ import {useNavigate, useLocation} from 'react-router';
           required:true,
           label:'What is your classification? ',
           placeholder: 'Type Here',
-          order: 7
+          order: 8
       },
           'custom:graduate': {
           key:'graduate',
@@ -55,7 +65,7 @@ import {useNavigate, useLocation} from 'react-router';
           required:true,
           label:'When do you graduate? ',
           placeholder: 'Type Here',
-          order: 8
+          order: 9
       },
           'custom:skills': {
           key:'skills',
@@ -63,7 +73,7 @@ import {useNavigate, useLocation} from 'react-router';
           required:true,
           label:'List your skills',
           placeholder: 'Proficient in C++, Graphic Design, Web Analytics, Etc.',
-          order: 9
+          order: 10
       },
 
 
@@ -71,14 +81,37 @@ import {useNavigate, useLocation} from 'react-router';
     },
   }
 
+const UserProfile = () => {
+  useEffect(() => {
+    const fetchUserAttributes = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        const attributes = user.attributes;
+        console.log("User attributes:", attributes);
+      } catch (error) {
+        console.error("Failed to fetch user attributes.", error);
+      }
+    };
+
+    fetchUserAttributes();
+  }, []);
+
+  return (
+    <div>
+      {/* Render user profile */}
+    </div>
+  );
+};
 
 const Login = ({}) => {
     return (
         <Authenticator formFields={formFields}>
+            <UserProfile /> {/* Render UserProfile component */}
             {({signOut}) => {
                 <div>
                     <h1> yeehaw </h1>
                     <h3> this is a test </h3>
+
                     <button onClick={signOut}>Sign Out</button>
                 </div>
             }}
@@ -87,125 +120,3 @@ const Login = ({}) => {
 };
 
 export default Login;
-
-
-//my prev code
-
-// import React, {useState} from "react";
-// import styled from "styled-components";
-// import {InputAdornment, Button, TextField, Input} from "@mui/material";
-// import Visibility from "@mui/icons-material/Visibility";
-
-// import "./login.css";
-
-// export const Login = ({}) => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     function changeEmail(val) {
-//         setEmail(val)
-//     }
-
-//     function changePassword(val) {
-//         setPassword(val)
-//     }
-
-//     function sendLoginInfoToBackend() {
-//         // Send a POST request to the Django server
-//         fetch('http://127.0.0.1:8000/login_request/', {
-//             method: 'POST',
-//             withCredentials: true,
-//             mode: "cors",
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({email: email, password: password})
-//         })
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data)
-//             })
-//             .catch(error => {
-//                 console.log(error)
-//             });
-//     }
-
-//     const FlexColumn1Function = (e, name) => {
-//         alert(`${name} was clicked`);
-//     };
-//     return (
-//         <div id="LoginPage">
-//             <div id="Background">
-//                 <div id="Panel">
-//                     <text10>Welcome Back!</text10>
-
-//                     <text1>Sign in</text1>
-//                     <text2>to your Locol Student account</text2>
-//                     <text3>Email:</text3>
-//                     <div className="tf-outer">
-//                         <TextField
-//                             className="tf-inner"
-//                             variant="outlined"
-//                             position="absolute"
-//                             onChange={(e) => changeEmail(e.target.value)}
-//                         />
-
-
-//                     </div>
-//                     <text4>Password</text4>
-//                     <div className="tf-outer">
-//                         <TextField
-//                             className="tf-inner"
-//                             variant="outlined"
-//                             position="absolute"
-//                             InputProps={{
-//                                 endAdornment: (<InputAdornment position="end" style={{cursor: "pointer"}}>
-//                                     <Visibility
-//                                         onMouseDown={() => this.toggleShowPassword()}
-//                                         onMouseUp={() => this.toggleShowPassword()}
-//                                     />
-//                                 </InputAdornment>)
-//                             }}
-//                             onChange={(e) => changePassword(e.target.value)}
-//                             // InputProps={{
-//                             //   endAdornment: (
-//                             //     <InputAdornment position="end" style={{ cursor: "pointer"}}>
-//                             //       <Visibility
-//                             //         onMouseDown={() => this.toggleShowPassword()}
-//                             //         onMouseUp={() => this.toggleShowPassword()}
-//                             //       />
-//                             //     </InputAdornment>
-//                             //   ),
-//                             // }}
-//                             // type={this.state.showPassword ? "text" : "password"}
-//                             // value={this.state.password}
-//                             // onChange={(e) => this.changePassword(e.target.value)}
-//                         />
-
-//                         {/*
-
-//                     <Element1 />
-//                     <Text4>Password</Text4>
-//                     <FlexColumn>
-//                         <IconsaxLineareye
-//                             src={`https://file.rendit.io/n/InxojZGgvxtK7t5awRLt.svg`}
-//                         />
-//                     </FlexColumn> */}
-//                     </div>
-//                     <div className="FlexColumn1">
-//                         <button onClick={sendLoginInfoToBackend}>Login</button>
-//                     </div>
-//                     <text6>Don't have an account?
-//                         <a href="/signup_person"> Sign Up</a> instead.
-//                         <div><a href="/home">Forgot Password?</a></div> {/*change the url*/}
-//                     </text6>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// const IconsaxLineareye = styled.img`
-//   width: 100%;
-//   height: 24px;
-// `;
